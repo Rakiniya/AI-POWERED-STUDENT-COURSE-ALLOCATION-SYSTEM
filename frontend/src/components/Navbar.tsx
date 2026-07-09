6e0e449afd38d00afb7e9833ee1557078b9ced77
Rakiniya <kit.25.21bad042@gmail.com>
2026-07-09 12:00:56 +0530

    navbar icons

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,20 +6,43 @@ import {
   Box,
 } from "@mui/material";
 
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PeopleIcon from "@mui/icons-material/People";
 import SchoolIcon from "@mui/icons-material/School";
+import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
+import SmartToyIcon from "@mui/icons-material/SmartToy";
 
 import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
   const location = useLocation();
 
-  const menuItems = [
-  { label: "Dashboard", path: "/" },
-  { label: "Students", path: "/students" },
-  { label: "Courses", path: "/courses" },
-  { label: "Preferences", path: "/preferences" },
-  { label: "Allocation", path: "/allocation" },
-  { label: "AI Assistant", path: "/ai" },
+ const menuItems = [
+  {
+    label: "Dashboard",
+    path: "/",
+    icon: <DashboardIcon />
+  },
+  {
+    label: "Students",
+    path: "/students",
+    icon: <PeopleIcon />
+  },
+  {
+    label: "Courses",
+    path: "/courses",
+    icon: <SchoolIcon />
+  },
+  {
+    label: "Allocation",
+    path: "/allocation",
+    icon: <AssignmentTurnedInIcon />
+  },
+  {
+    label: "AI Assistant",
+    path: "/ai",
+    icon: <SmartToyIcon />
+  },
 ];
   return (
     <AppBar
@@ -50,25 +73,28 @@ function Navbar() {
         {menuItems.map((item) => (
 
           <Button
-            key={item.path}
-            component={Link}
-            to={item.path}
-            color="inherit"
-            sx={{
-              mx: 1,
-              borderBottom:
-                location.pathname === item.path
-                  ? "3px solid white"
-                  : "none",
-              borderRadius: 0,
-              fontWeight:
-                location.pathname === item.path
-                  ? "bold"
-                  : "normal",
-            }}
-          >
-            {item.label}
-          </Button>
+  key={item.path}
+  component={Link}
+  to={item.path}
+  color="inherit"
+  sx={{
+    mx:1,
+    display:"flex",
+    alignItems:"center",
+    gap:0.5,
+    borderBottom:
+      location.pathname === item.path
+      ? "3px solid white"
+      : "none",
+    borderRadius:0,
+  }}
+>
+
+  {item.icon}
+
+  {item.label}
+
+</Button>
 
         ))}
 
